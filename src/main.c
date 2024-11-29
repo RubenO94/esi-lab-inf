43b9b45348fb72ae8098152a831e00fee28778f6
Raúl Ribeiro <rauloctober7@hotmail.com>
2024-11-29 19:32:16 +0000

    First commit

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/**
+ * @file main.c
+ * @author Raúl Ribeiro ({a22552@alunos.ipca.pt})
+ * @brief The main application file.
+ * @version 0.1
+ * @date 2024-11-29
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main( int argc, char *argv[]) {
+
+}
